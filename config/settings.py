b701ffe09efee75acc769bb3577b3dafbd8e9120
sanaipei001 <lenapunyasanaipei@gmail.com>
2025-03-21 18:39:42 +0300

    Update ALLOWED_HOSTS for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9g4_@r*l&bw(ngafy$%q4u@zvm(t))-i-f^99h@kk3i6zuo(+s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-starter-template.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
